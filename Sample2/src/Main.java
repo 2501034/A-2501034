ba08fa15e62070ec40fc85bfe61228e1ecc16f20
2501034 <skytimber35@kbu.ac.kr>
2025-09-10 15:51:52 +0900

    Add files via upload

---
+++ Sample2/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        //준비물
+        Scanner keyboard = new Scanner(System.in);//new 생성자
+        String name = "";
+
+        //Data 입력
+
+        System.out.print("당신의 이름은 ? ");
+        name = keyboard.nextLine();
+
+        //처리
+
+
+        //Information 출력 (필수)
+        System.out.println("이름 : " + name);
+
+    }
+}
